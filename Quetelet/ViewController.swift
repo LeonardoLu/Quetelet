8715b5d6ae3f4969a727ee8fd547b2198428e629
LeonardoLu <leonardolu1.618@outlook.com>
2020-10-05 20:04:41 +0800

    Initial Commit

---
+++ Quetelet/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Quetelet
+//
+//  Created by lujunhui on 2020/10/5.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
